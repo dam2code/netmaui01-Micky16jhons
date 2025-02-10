3358942afa3b29515d421a95c03c9dca574c0bd5
Micky16jhons <micky.riera@educa.madrid.org>
2025-02-10 19:13:11 +0100

    Ejercicio4.2-Bien echo

--- exercise2/TipCalculator/AppShell.xaml.cs
+++ exercise2/TipCalculator/AppShell.xaml.cs
@@ -5,5 +5,9 @@ public partial class AppShell : Shell
     public AppShell()
     {
         InitializeComponent();
+
+        Routing.RegisterRoute(nameof(CustomTipPage), typeof(CustomTipPage));
+        
+        Application.Current.UserAppTheme = AppTheme.Light;
     }
 }
