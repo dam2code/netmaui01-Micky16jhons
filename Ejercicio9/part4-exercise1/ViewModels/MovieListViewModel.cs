89c44bb3f9d136ca6e38a80496af7b308fd82e88
Micky16jhons <micky.riera@educa.madrid.org>
2025-02-14 03:15:18 +0100

    Ejercicio9.1-bien

---
+++ Ejercicio9/part4-exercise1/ViewModels/MovieListViewModel.cs
@@ -0,0 +1,33 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace MovieCatalog.ViewModels;
+
+public class MovieListViewModel : ObservableObject
+{
+    private MovieViewModel _selectedMovie;
+
+    public ObservableCollection<MovieViewModel> Movies { get; }
+
+    public MovieViewModel SelectedMovie
+    {
+        get => _selectedMovie;
+        set => SetProperty(ref _selectedMovie, value);
+    }
+    public object MovieService { get; }
+
+    public MovieListViewModel()
+    {
+        Movies = new ObservableCollection<MovieViewModel>(MovieService.LoadMovies());
+    }
+
+    internal void DeleteMovie(MovieViewModel movie)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal async Task<object> RefreshMovies()
+    {
+        throw new NotImplementedException();
+    }
+}
